0522a5d0ad27e80f6a4d3b8c252573b1b1d0a48c
SivakumarM567 <shivakumar85249@gmail.com>
2022-05-05 14:20:47 +0530

    Commit

---
+++ OnlineJobPortal.UI/Controllers/JobseekersDetailsController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using OnlineJobPortal.Entity;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace OnlineJobPortal.UI.Controllers
+{
+    public class JobseekersDetailsController : Controller
+    {
+        private IConfiguration _configuration;
+        public JobseekersDetailsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Save()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Save(JobseekersDetails jobseekersDetails)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(jobseekersDetails), Encoding.UTF8, "application/json");
+                string endPoint = _configuration["WebApiBaseUrl"] + "JobseekersDetails/Save";
+                using (var response = await client.PostAsync(endPoint, content))
+                {
+                    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        ViewBag.status = "Ok";
+                        ViewBag.message = "Jobseeker details saved successfully!";
+                    }
+                    else
+                    {
+                        ViewBag.status = "Error";
+                        ViewBag.message = "Wrong entries!";
+                    }
+                }
+            }
+            return View();
+        }
+    }
+}
